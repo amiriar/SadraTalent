90e3d11ca07f6c317000b96a4bc8b6166c9cc9eb
amiriar <amiriar.irr@gmail.com>
2025-01-20 11:52:57 +0330

    Add Message schema for handling chat messages in the application

---
+++ Backend/src/api/admin/messages/messagesSchema.ts
@@ -0,0 +1,66 @@
+import mongoose, { Document, ObjectId, Schema } from "mongoose";
+
+export interface IUserInfo {
+  _id: ObjectId | string;
+  username: string;
+  profile: string;
+  phone: string;
+}
+
+export interface IMessage extends Document {
+  sender: IUserInfo;
+  aspSender: IUserInfo;
+  receiver: IUserInfo;
+  aspReceiver: IUserInfo;
+  content: string;
+  room: string;
+  // timestamp: Date;
+  // date: string;
+  status: "sent" | "delivered" | "seen";
+  voiceUrl?: string;
+  fileUrl?: string;
+  isEdited: boolean;
+  isPinned: boolean;
+  isDeleted: boolean;
+  isForwarded: boolean;
+  isDeletedForMe: boolean;
+  replyTo: ObjectId | string;
+  forwardedFrom?: ObjectId | string;
+  deletedBy?: ObjectId | string;
+  storyId?: ObjectId | string;
+}
+
+const MessageSchema = new Schema<IMessage>(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, required: false },
+    room: { type: String, required: true },
+    // date: { type: String, required: false },
+    status: {
+      type: String,
+      enum: ["sent", "delivered", "seen"],
+      default: "sent",
+    },
+    voiceUrl: { type: String, required: false },
+    fileUrl: { type: String, required: false },
+    isPinned: { type: Boolean, default: false },
+    isEdited: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+    isForwarded: { type: Boolean, default: false },
+    replyTo: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    forwardedFrom: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    deletedBy: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User", default: [] },
+    ],
+    storyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const MessageModel = mongoose.model<IMessage>("Message", MessageSchema);
+export default MessageModel;
