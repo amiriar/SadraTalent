10f906a46f3cb8ed2f17b0949f444bc538dcf952
amiriar <amiriar.irr@gmail.com>
2025-02-04 13:08:49 +0330

    Refactor AdminStoriesRepository with CRUD methods for story management

--- Backend/src/api/admin/stories/storiesRepository.ts
+++ Backend/src/api/admin/stories/storiesRepository.ts
@@ -1,14 +1,34 @@
 import StoryModel, { IStory } from "./storiesSchema";
 
-export class StoriesRepository {
-  async getAllStories(page: string, limit: string): Promise<IStory[] | null> {
-    const skip = (parseInt(page) - 1) * parseInt(limit);
-    return await StoryModel.find({}).skip(skip).limit(parseInt(limit));
+export class AdminStoriesRepository {
+  async getAllStories(
+    page: string,
+    limit: string,
+    isDeleted: boolean
+  ): Promise<IStory[] | null> {
+    const skip = parseInt(page) * parseInt(limit) - parseInt(limit);
+    return await StoryModel.find({ isDeleted })
+      .skip(skip)
+      .limit(parseInt(limit))
+      .exec();
   }
 
-  async getStoriesById(roomId: string): Promise<IStory | null> {
-    return await StoryModel.findOne({
-      _id: roomId,
-    });
+  async getStoriesById(storyId: string): Promise<IStory | null> {
+    return await StoryModel.findById(storyId).exec();
+  }
+
+  async deleteStoryById(storyId: string): Promise<IStory | null> {
+    return await StoryModel.findByIdAndUpdate(storyId, {
+      isDeleted: true,
+    }).exec();
+  }
+
+  async updateStoryById(
+    storyId: string,
+    newData: IStory
+  ): Promise<IStory | null> {
+    return await StoryModel.findByIdAndUpdate(storyId, newData, {
+      new: true,
+    }).exec();
   }
 }
