87e2118bfe41565f9c42705b287acfb2fdd190c7
amiriar <amiriar.irr@gmail.com>
2025-02-15 08:28:26 +0330

    Refactor AuthGuard to simplify token verification and error handling

--- Backend/src/common/guard/AuthGuard.ts
+++ Backend/src/common/guard/AuthGuard.ts
@@ -4,6 +4,7 @@ import { logger } from "@/server";
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
+import createHttpError from "http-errors";
 
 // Updated response handler function using ServiceResponse
 const handleResponse = (res: Response, status: number, message: string) => {
@@ -11,29 +12,45 @@ const handleResponse = (res: Response, status: number, message: string) => {
   return res.status(status).send(serviceResponse);
 };
 
-export const AuthGuard = async (req: Request, res: Response, next: NextFunction) => {
+export const AuthGuard = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return handleResponse(res, StatusCodes.UNAUTHORIZED, "Unauthorized: No token provided");
+      return handleResponse(
+        res,
+        StatusCodes.UNAUTHORIZED,
+        "Unauthorized: No token provided"
+      );
     }
 
     const token = authHeader.split(" ")[1];
 
     // Verify token
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as { _id: string };
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { _id: string; role: string };
 
-    // Find user from token payload
-    const user = await UserModel.findById(decodedToken._id);
-    if (!user) {
-      return handleResponse(res, StatusCodes.UNAUTHORIZED, "Unauthorized: Invalid token");
-    }
+    // // Find user from token payload
+    // const user = await UserModel.findById(decodedToken._id);
+    // if (!user) {
+    //   return handleResponse(res, StatusCodes.UNAUTHORIZED, "Unauthorized: Invalid token");
+    // }
 
     // Attach user to the request
-    req.user = user;
+    req.user = decodedToken;
     next();
   } catch (error: any) {
     logger.error(`AuthGuard Error: ${error.message}`);
-    return handleResponse(res, StatusCodes.UNAUTHORIZED, "Unauthorized: Invalid or expired token");
+    next(createHttpError.Unauthorized("1وارد حساب کاربری خود شوید"));
+    return handleResponse(
+      res,
+      StatusCodes.UNAUTHORIZED,
+      "Unauthorized: Invalid or expired token"
+    );
   }
 };
